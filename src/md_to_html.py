02c93d73ea1566c743dc1a207b32d76ecc51c049
Paulose Kurian <paulose610@gmail.com>
2026-01-10 23:36:38 +0530

    add md to html_node

---
+++ src/md_to_html.py
@@ -0,0 +1,92 @@
+from src.block_helper import block_to_block_type, BlockType, markdown_to_blocks
+from src.htmlnode import LeafNode, ParentNode
+from src.inline_helper import text_to_textnodes, text_node_to_html_node
+#from src.textnode import TextNode, TextType
+
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    block_nodes = process_blocks(blocks)
+
+    html_node = ParentNode(tag='div')
+    html_node.children = block_nodes
+    
+    return html_node
+
+
+def process_blocks(blocks):
+    block_nodes = []
+    
+    for i in blocks:
+        block_type = block_to_block_type(i)
+        block_node=None
+        block_children=None
+        match block_type:
+            case BlockType.PARA:
+                block_node = ParentNode(tag='p')
+                block_children = get_block_children(i,BlockType.PARA)
+                block_node.children = block_children
+            case BlockType.CODE:
+                block_node = ParentNode(tag='pre',val=None,children=[LeafNode(tag='code',val=i.split('```',2)[1][1:])])
+            case BlockType.HEADING:
+                head_count = 0
+                for c in i:
+                    if c=='#':
+                        head_count+=1
+                    if c=='\n':
+                        break
+                head_tag = f'h{head_count}'
+                block_node = ParentNode(tag=head_tag)
+                block_children = get_block_children(i,BlockType.HEADING)
+                block_node.children = block_children
+            case BlockType.UO_LIST:
+                block_node = ParentNode(tag='ul')
+                block_children = get_block_children(i,BlockType.UO_LIST)
+                block_node.children = block_children
+            case BlockType.O_LIST:
+                block_node = ParentNode(tag='ol')
+                block_children = get_block_children(i,BlockType.O_LIST)
+                block_node.children = block_children
+            case BlockType.QUOTE:
+                block_node = ParentNode(tag='quote')
+                block_children = get_block_children(i,BlockType.QUOTE)
+                block_node.children = block_children
+            
+        block_nodes.append(block_node)
+    
+    return block_nodes
+
+
+def get_block_children(block,block_type):
+    children = []
+    for i in block.split('\n'):
+        i_node = None
+        i_children = []
+        match block_type:
+            case BlockType.PARA:
+                i_children = text_to_textnodes(i)
+                i_node = ParentNode(tag=None,val=None) if len(i_children)>1 else LeafNode(tag=None,val=i)
+            case BlockType.HEADING:
+                i=i.split(' ',1)[1]
+                i_children = text_to_textnodes(i)
+                i_node = ParentNode(tag=None,val=None) if len(i_children)>1 else LeafNode(tag=None,val=i)
+            case BlockType.UO_LIST:
+                i=i.split(' ',1)[1]
+                i_children = text_to_textnodes(i)
+                i_node = ParentNode(tag='li',val=None) if len(i_children)>1 else LeafNode(tag='li',val=i)
+            case BlockType.O_LIST:
+                i=i.split(' ',1)[1]
+                i_children = text_to_textnodes(i)
+                i_node = ParentNode(tag='li',val=None) if len(i_children)>1 else LeafNode(tag='li',val=i)
+            case BlockType.QUOTE:
+                i=i.split(' ',1)[1]
+                i_children = text_to_textnodes(i)
+                i_node = ParentNode(tag=None,val=None) if len(i_children)>1 else LeafNode(tag=None,val=i)
+                
+        if i_node:
+            if len(i_children)>1:
+                i_children = list(map(lambda x: text_node_to_html_node(x), i_children))
+                i_node.children = i_children
+        children.append(i_node)
+    return children
+            
---
+++ src/tests/test_md_to_html.py
@@ -0,0 +1,187 @@
+import unittest
+import pprint as p
+
+from src.md_to_html import markdown_to_html_node
+
+class test_md_to_html(unittest.TestCase):
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
+    
+    def test_multiple_paragraphs(self):
+        md = """
+First paragraph line one
+line two
+
+Second paragraph here
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        
+        self.assertEqual(
+            html,
+            "<div><p>First paragraph line one line two</p><p>Second paragraph here</p></div>",
+        )
+
+
+    def test_unordered_list(self):
+        md = """
+- Item one
+- Item two
+- Item three
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        
+        self.assertEqual(
+            html,
+            "<div><ul><li>Item one</li><li>Item two</li><li>Item three</li></ul></div>",
+        )
+
+
+    def test_ordered_list(self):
+        md = """
+1. First
+2. Second
+3. Third
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        
+        self.assertEqual(
+            html,
+            "<div><ol><li>First</li><li>Second</li><li>Third</li></ol></div>",
+        )
+
+
+    def test_blockquote(self):
+        md = """
+> This is a quote
+> that spans lines
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        
+        self.assertEqual(
+            html,
+            "<div><blockquote>This is a quote that spans lines</blockquote></div>",
+        )
+
+
+    def test_mixed_blocks(self):
+        md = """
+Intro paragraph
+
+- List item one
+- List item two
+
+> Quoted text here
+
+Final paragraph
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        
+        self.assertEqual(
+            html,
+            "<div><p>Intro paragraph</p><ul><li>List item one</li><li>List item two</li></ul><blockquote>Quoted text here</blockquote><p>Final paragraph</p></div>",
+        )
+
+    def test_all_block_types(self):
+        self.maxDiff = None
+        md = """
+This is the _opening paragraph_.
+It spans multiple lines **but** should be treated as a single paragraph.
+
+This is the second paragraph, separated by an image ![you](www.link).
+
+### teh fitr ordered list
+
+- _First_ unordered item
+- Second unordered item
+- Third unordered item
+
+## thekdfs
+##### hu
+
+1. First **ordered** item
+2. Second ordered item with ![image](link)
+3. Third ordered item 
+
+> This is a block quote
+> that spans multiple [lines](link)
+> and should be merged into one paragraph.
+"""
+
+        node = markdown_to_html_node(md)
+        #print(node)
+        html = node.to_html()
+
+        self.assertEqual(
+            html,
+            "<div>"
+            "<p>This is the <i>opening paragraph</i>. It spans multiple lines <b>but</b> should be treated as a single paragraph.</p>"
+            "<p>This is the second paragraph, separated by an image <img src='www.link' alt='you'/>.</p>"
+            "<h3>teh fitr ordered list</h3>"
+            "<ul><li><i>First</i> unordered item</li><li>Second unordered item</li><li>Third unordered item</li></ul>"
+            "<h2>thekdfs</h2>"
+            "<ol><li>First <b>ordered</b> item</li><li>Second ordered item with <img src='link' alt='image'/></li><li>Third ordered item</li></ol>"
+            "<blockquote>This is a block quote that spans multiple <a href='link'>lines</a> and should be merged into one paragraph.</blockquote>"
+            "</div>",
+        )
+
+    def test_all_block_types_2(self):
+        self.maxDiff = None
+        md = """
+This is the _opening paragraph_.
+It spans multiple lines **but** should be treated as a single paragraph.
+
+This is the second paragraph, separated by an image ![you](www.link).
+"""
+
+        node = markdown_to_html_node(md)
+        #print(node)
+        html = node.to_html()
+
+        self.assertEqual(
+            html,
+            "<div>"
+            "<p>This is the <i>opening paragraph</i>. It spans multiple lines <b>but</b> should be treated as a single paragraph.</p>"
+            "<p>This is the second paragraph, separated by an image <img src='www.link' alt='you'/>.</p>"
+            "</div>",
+        )
