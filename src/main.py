a7e7869b32550eb5fe5273f4235588f5546d6587
Paulose Kurian <paulose610@gmail.com>
2026-01-12 03:48:53 +0530

    configured hosting in github pages

--- src/main.py
+++ src/main.py
@@ -1,13 +1,27 @@
 import os
+import sys
 
-from main_helper import clean_folder, move_stuff, generate_page
+from main_helper import clean_folder, move_stuff, generate_pages_recursive
 
 def main():
-    if not os.path.exists('public'):
-        os.mkdir('public')
-    clean_folder('public')
-    move_stuff('static','public')
-    generate_page('content/index.md','template.html','public/index.html')
+    root_path = sys.argv[1] if len(sys.argv)>1 else '/'
+    output_path = sys.argv[2] if len(sys.argv)>2 else 'docs'
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+    
+    clean_folder(output_path)
+    move_stuff('static',output_path)
+    
+    content_path_abs = os.path.abspath('content/')
+    dest_path_abs = os.path.abspath(output_path)      
+    
+    print(f"Generating pages from content/ to {output_path}/ using template.html")
+    
+    with open('template.html','r') as f:
+        templet_cont = f.read()
+
+    generate_pages_recursive(root_path,content_path_abs,templet_cont,dest_path_abs)
+    #generate_pages_recursive('content/index.md','template.html','public/index.html')
 
 if __name__ == '__main__':
     main()
--- src/main_helper.py
+++ src/main_helper.py
@@ -4,10 +4,9 @@ import shutil
 from helper.md_to_html import markdown_to_html_node
 
 def clean_folder(folder):
+    #print('cleaning public folder')
     folder_path = os.path.abspath(folder)
-    print(folder_path)
     dirs = os.listdir(folder_path)
-    print(dirs)
     for i in dirs:
         i_path = os.path.join(folder_path,i)
         if i!='__init__.py' and os.path.isfile(i_path):
@@ -18,11 +17,10 @@ def clean_folder(folder):
 
 
 def move_stuff(here,there):
+    #print('moving static content from static to public')
     folder_path = os.path.abspath(here)
     dest_path = os.path.abspath(there)
-    print(folder_path)
     dirs = os.listdir(folder_path)
-    print(dirs)
     for i in dirs:
         i_path = os.path.join(folder_path,i)
         if i!='__init__.py' and os.path.isfile(i_path):
@@ -41,15 +39,33 @@ def extract_title(markdown):
         raise ValueError("Title not found in the markdown.")
     
 
-def generate_page(from_path, template_path, dest_path):
-    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+# def generate_page(from_path, template_path, dest_path):
+#     md_cont = None
+#     try:
+#         with open(from_path,'r') as f:
+#             md_cont = f.read()
+#         with open(template_path,'r') as f:
+#             template_cont = f.read()    
+    
+#     except Exception as e:
+#         raise ValueError(e)
+    
+#     md_html = markdown_to_html_node(md_cont).to_html()
+    
+#     actual_title = extract_title(md_cont)
+
+#     template_cont = template_cont.replace("{{ Title }}", actual_title)
+#     template_cont = template_cont.replace("{{ Content }}", md_html)
+
+#     with open(dest_path, 'w') as f:
+#         f.write(template_cont)
+
+def generate_page(root_path,from_path, template_cont, dest_path):
     md_cont = None
+    print(f"Generating page from {from_path.split('/')[-2:]}")
     try:
         with open(from_path,'r') as f:
             md_cont = f.read()
-        with open(template_path,'r') as f:
-            template_cont = f.read()    
-    
     except Exception as e:
         raise ValueError(e)
     
@@ -59,6 +75,20 @@ def generate_page(from_path, template_path, dest_path):
 
     template_cont = template_cont.replace("{{ Title }}", actual_title)
     template_cont = template_cont.replace("{{ Content }}", md_html)
+    template_cont = template_cont.replace("href='/", f"href='{root_path}")
+    template_cont = template_cont.replace("src='/", f"src='{root_path}")
 
     with open(dest_path, 'w') as f:
         f.write(template_cont)
+
+def generate_pages_recursive(root_path,content_dir_path, template_cont, dest_dir_path):
+    dir_cont = os.listdir(content_dir_path)
+    for i in dir_cont:
+        i_path = os.path.join(content_dir_path,i)
+        if os.path.isfile(i_path):
+            des_path = os.path.join(dest_dir_path,i.split('.')[0]+'.html')
+            generate_page(root_path,i_path,template_cont,des_path)
+        elif os.path.isdir(i_path):
+            des_path = os.path.join(dest_dir_path,i)
+            os.mkdir(des_path)
+            generate_pages_recursive(root_path,i_path,template_cont,des_path)
